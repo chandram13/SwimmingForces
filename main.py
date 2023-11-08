22153ce9d713d866f222f9c5dd151e00fa6e1929
Marvish Subhas Chandra <mschandr@uci.edu>
2023-11-08 15:47:43 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def generaLFormula(distance,armCycles,cycles,time):
     strokeRate = cycles / time
     secondsStroke = time/ cycles
 
+    print(distanceperstroke)
+    print(strokeRate)
+    print(secondsStroke)
+
 
 # algebraic expression behind swimming
 # d, total drag
@@ -20,8 +24,22 @@ def generaLFormula(distance,armCycles,cycles,time):
 
 def totalDrag(D,q,SA,Q,fD,V,K,W):
     swimmerDrag = [((D / (q * SA)) * (1 / 2 * Q * (V * V)) * SA) + (K * (W * W) / (1 / 2 * fD * V * V) * SA)]
+    print(swimmerDrag)
 
+# D, drag
+# F, force
+# R, turnover rate
+
+# Applying arm acceleration for greatest velocity equation
+def armAccel(D,F,R):
+    swimmingSpeed = F * R - D
+    print(swimmingSpeed)
 
 # calculating buoyancy for a swimmer, force behind swimmer to float and not sink (larger the better)
 # p = fluid density, V = volume, g = acceleration (-9.8)
-buoyancy = p * V * g
+
+def buoyancy(p,V,g):
+    givenBuoyancy = p * V * g
+    print(givenBuoyancy)
+
+
